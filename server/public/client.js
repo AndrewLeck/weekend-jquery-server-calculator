5f6182ac19e7e3c9321f54419285c614921d9092
Andrew Leck <Aleck360@gmail.com>
2022-05-20 11:12:18 -0500

    set up main frame work

--- server/public/client.js
+++ server/public/client.js
@@ -1,9 +1,16 @@
 console.log('hello world!')
-
 $(document).ready(readyNow);
 
 
 function readyNow(){
  console.log('JQ LOADED');
+
+ $('#total-number').on('click', getTotalNumber);
 }
 
+
+
+function getTotalNumber(){
+
+    
+}
